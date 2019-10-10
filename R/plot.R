a094be568c6922753b18031a3d8c1f7c70d4efac
WUR\berg270 <sanne1.vandenberg@wur.nl>
2019-10-10 14:29:34 +0200

    Corrected a mistake in the input data for the kde2d function

--- R/plot.R
+++ R/plot.R
@@ -14,7 +14,7 @@ plot_positions <- function(data_loc, type='scatter') {
   if (type=='scatter') {
     p <- plot(data_loc$x, data_loc$y, pch=19, col=rgb(0, 0, 0, 0.15))
   } else if (type == 'density') {
-    k <- kde2d(data$x, data$y, n=100)
+    k <- kde2d(data_loc$x, data_loc$y, n=100)
     p <- image(k, col = topo.colors(100))
   }
 
