0c03d429b555b9c2609f562ffe84a8811e99637b
WUR\berg270 <sanne1.vandenberg@wur.nl>
2019-10-14 09:37:11 +0200

    Added the first real data
    Made a new script to perform the first real analysis

---
+++ scripts/first_analysis.R
@@ -0,0 +1,23 @@
+## Clear environment
+rm(list=ls())
+
+# Load libraries
+library(dplyr)
+library(MASS)
+
+## Source all functions
+sapply(list.files('R', full.names = T), source)
+
+# List files
+files <- list.files('data/2019-07-31', full.names = T)
+files <- files[grep('raw_0001.csv', files, value = F)]
+
+# Loop through all files
+for(file.nr in 1:length(files)){
+  # Import data
+  data <- import(files[[file.nr]])
+  # Apply next steps to all locations, so make list of locations
+  list_locations <- as.list(unique(data$location))
+  # Filter on location
+  data <- lapply(list_locations, function(x){ filter_data(data, filter_location = x) })
+}
