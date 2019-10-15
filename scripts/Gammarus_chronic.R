493e5b6994d8811f8521be848ffae2187aac6ce2
WUR\berg270 <sanne1.vandenberg@wur.nl>
2019-10-15 17:20:19 +0200

    Minor changes to run snail script

--- scripts/Gammarus_chronic.R
+++ scripts/Gammarus_chronic.R
@@ -7,13 +7,13 @@ library(Hmisc)
 library(tidyr)
 
 # Load input data
-load('output/2019-07-31.Rda')
+load('output/2019-08-01.Rda')
 
 # Only focus on FLU data
 data <- filter(output_data, Treatment_chem == 'FLU' | Treatment_chem == 'C')
 
 # Create bins/ 10 second groups
-bins <- seq(0, 480, 1)
+bins <- seq(0, 240, 5) # 480 for 8 min
 # Add groups to data
 data$group <- cut(data$time, bins, labels = FALSE)
 # Add interaction between timebin and treatment group
@@ -25,15 +25,10 @@ summarised_data <- data %>% group_by(combined_group) %>%
             group = mean(group),
             Treatment_conc = mean(Treatment_conc))
 # Remove strange row
-summarised_data <- summarised_data[-2401,]  # TODO check how this row is made
+summarised_data <- summarised_data[-241,]  # TODO check how this row is made
 
 # Convert into wide format, using 10s timebins as columns
 summarised_data <- spread(summarised_data[2:4], group, mean.speed)
-
-# Add treatments as rownames
-rownames(summarised_data) <- summarised_data$Treatment_conc
-# Remove treatments from df
-summarised_data <- summarised_data[-1]
 
 # Plot heatmap
 heatmap(as.matrix(summarised_data[-1]), Colv = NA, Rowv = NA, scale = 'row',
