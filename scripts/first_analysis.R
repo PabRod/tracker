7e69b443a5eb774d553dbb054329858d4682a461
WUR\berg270 <sanne1.vandenberg@wur.nl>
2019-10-15 16:07:45 +0200

    Added loop to go through different test dates

--- scripts/first_analysis.R
+++ scripts/first_analysis.R
@@ -8,42 +8,49 @@ library(MASS)
 # Source all functions
 sapply(list.files('R', full.names = T), source)
 
-# List files
-date <- '2019-07-31'
-files <- list.files(paste('data/', date, sep = ''), full.names = T)
-files <- files[grep('raw_0001.csv', files, value = F)]
+# Create list of test dates
+test_dates <- c('2019-07-31', '2019-08-01')
 
-# Create empty list to collect output data
-output <- list(0)
-
-# Loop through all files
-for(file.nr in 1:length(files)){
-  # Import data
-  data <- import(files[[file.nr]])
-  # Apply next steps to all locations, so make list of locations
-  list_locations <- as.list(unique(data$location))
-  # Filter on location
-  data <- lapply(list_locations, function(x){ filter_data(data, filter_location = x) })
-  # Append timebins
-  data <- lapply(data, function(x){ append_time_bins(x) })
-  # Append dynamics to all locations
-  data <- lapply(data, function(x){ append_dynamics(x) })
-  # Append polar coordinates and make histogram of radius distribution
-  data <- lapply(data, function(x){ append_polar_coordinates(x) })
-  # Add cosm nr to data
-  data <- lapply(data, function(x){ append_exp_info(x, files[[file.nr]]) })
-  # Collect all data together
-  data <- do.call('rbind', data)
-  # Store in output list
-  output[[file.nr]] <- data
+# Loop through test dates
+for(test.date in 1:length(test_dates)){
+  
+  # List files
+  date <- test_dates[[test.date]]
+  files <- list.files(paste('data/', date, sep = ''), full.names = T)
+  files <- files[grep('raw_0001.csv', files, value = F)]
+  
+  # Create empty list to collect output data
+  output <- list(0)
+  
+  # Loop through all files
+  for(file.nr in 2:length(files)){
+    # Import data
+    data <- import(files[[file.nr]])
+    # Apply next steps to all locations, so make list of locations
+    list_locations <- as.list(unique(data$location))
+    # Filter on location
+    data <- lapply(list_locations, function(x){ filter_data(data, filter_location = x) })
+    # Append timebins
+    data <- lapply(data, function(x){ append_time_bins(x) }) # TODO only for Gammarus
+    # Append dynamics to all locations
+    data <- lapply(data, function(x){ append_dynamics(x) })
+    # Append polar coordinates and make histogram of radius distribution
+    data <- lapply(data, function(x){ append_polar_coordinates(x) })
+    # Add cosm nr to data
+    data <- lapply(data, function(x){ append_exp_info(x, files[[file.nr]]) })
+    # Collect all data together
+    data <- do.call('rbind', data)
+    # Store in output list
+    output[[file.nr]] <- data
+  }
+  
+  # Combine all data together
+  output_data <- do.call('rbind', output)
+  # Load treatment data
+  treatments <- read.csv('data/treatments.csv')
+  # Add treatment data to rest of data
+  output_data <- merge(x = output_data, y = treatments, by.x = "cosm_nr", by.y = "Cosm")
+  
+  # Save data
+  save(output_data, file = paste('output/', date, '.Rda', sep = ''))
 }
-
-# Combine all data together
-output_data <- do.call('rbind', output)
-# Load treatment data
-treatments <- read.csv('data/treatments.csv')
-# Add treatment data to rest of data
-output_data <- merge(x = output_data, y = treatments, by.x = "cosm_nr", by.y = "Cosm")
-
-# Save data
-save(output_data, file = paste('output/', date, '.Rda', sep = ''))
